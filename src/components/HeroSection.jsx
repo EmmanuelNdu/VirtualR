f5cd0985024e00de173a865b982ffe0eb63931ca
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-24 10:00:17 +0100

    testing waters

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className='flex flex-col items-center mt-6 lg:mt-20'>
+        <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
+            VirtualR build tools
+            <span className="bg-gradient-to-r from-orange-500 to-red-800 bg-clip-text">
+                {" "}
+                for developers
+            </span>
+        </h1>
+    </div>
+  );
+};
 
 export default HeroSection
