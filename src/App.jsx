ac22f4f58e4178ec1c817761ec0fbe0f9fd1e234
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-24 14:56:03 +0100

    completed the Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <HeroSection />
+    <FeatureSection />
     </>
   )
 }
