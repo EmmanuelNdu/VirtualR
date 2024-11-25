2f651142886deab31404ec3be40bc08cf6faa466
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-25 03:04:46 +0100

    completed the Testimonial section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
 import WorkFlow from './components/WorkFlow'
 import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
     <FeatureSection />
     <WorkFlow />
     <Pricing />
+    <Testimonials />
     </>
   )
 }
