b3508b3ed60ef5a0b161fa35d56ec81d40aab66d
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-25 02:04:10 +0100

    completed the pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -28,11 +28,19 @@ const Pricing = () => {
                          </p>
                          <ul>
                             {Option.features.map((feature, index) =>(
-                                <li key={index}>
+                                <li key={index} className='mt-8 flex items-center'>
                                     <CheckCircle2 />
+                                    <span className='ml-2'>{feature}</span>
                                 </li>
                             ))}
                          </ul>
+                         <a href='#'
+                         className="inline-flex justify-center items-center text-center
+                         w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900
+                         border border-orange-900 rounded-lg transition duration-200"
+                         >
+                            Subscribe
+                         </a>
                     </div>
                 </div>
             ))}
